a82b41a74de5d7729a1348170458dc2ef7c7470f
gdghanu-mahn105 <gdschanu.manhdd1005@gmail.com>
2026-01-10 22:23:15 +0700

    (fix) : logging with aop

---
+++ forum/src/main/java/com/example/forum/aspect/EmailLoggingAspect.java
@@ -0,0 +1,38 @@
+package com.example.forum.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class EmailLoggingAspect {
+
+    @Pointcut(value = "execution(* com.example.forum.service.EmailService.*(..))")
+    public void emailServiceMethods(){}
+
+    @Before("emailServiceMethods()")
+    public void logBefore(JoinPoint joinPoint){
+        log.info("[Email] Initiating email request: '{}' with arguments: {}",
+                joinPoint.getSignature().getName(),
+                Arrays.toString(joinPoint.getArgs()
+                ));
+    }
+
+    @AfterReturning("emailServiceMethods()")
+    public void logSuccess(JoinPoint joinPoint) {
+        log.info("[EMAIL] Email request processed successfully: '{}'",
+                joinPoint.getSignature().getName());
+    }
+
+    @AfterThrowing(pointcut = "emailServiceMethods()", throwing = "ex")
+    public void logError(JoinPoint joinPoint, Exception ex){
+        log.error("[EMAIL] Failed to process email request: '{}'. Error reason: {}",
+                joinPoint.getSignature().getName(),
+                ex.getMessage());
+    }
+}
--- forum/src/main/java/com/example/forum/service/impl/EmailServiceImpl.java
+++ forum/src/main/java/com/example/forum/service/impl/EmailServiceImpl.java
@@ -27,17 +27,14 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendSimpleMessageMail(String to, String subject, String text) {
         try {
-
             SimpleMailMessage mail = new SimpleMailMessage();
             mail.setTo(to);
             mail.setSubject(subject);
             mail.setText(text);
 
             javaMailSender.send(mail);
-            System.out.println("Đã gửi mail đến: "+ to);
-
         } catch (Exception e) {
-            System.out.printf("Lỗi khi gửi mail: ", e.getMessage());
+            e.printStackTrace();
         }
     }
 
